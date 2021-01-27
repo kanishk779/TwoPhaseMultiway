e86854a77fb0fcf487638d829ccf0793c2e8d65d
kanishk779 <kashyapamankumar54@gmail.com>
2021-01-27 14:09:03 +0530

    Add write_temp_file function

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from collections import OrderedDict
 
 
 class TwoPhaseSort:
-    def __init__(self, input, output, column_list, main_memory):
+    def __init__(self, input_file, output, column_list, main_memory):
         self.info_file = OrderedDict()
+        self.input_file = input_file
+        self.output_file = output
+        self.column_list = column_list
+        self.main_memory = main_memory
+        self.buffer = []  # this is used to keep the unsorted data chunk for writing to temp files
 
     def meta_info(self):
         """
@@ -25,4 +30,28 @@ class TwoPhaseSort:
                 col_size = int(data[1].strip())
                 self.info_file[col_name] = col_size
 
-    
+    def write_temp_file(self, index):
+        """
+        It writes the current sorted buffer into a temporary file, and clears the buffer for future operations
+        :param index: used for naming the current file by adding index as suffix
+        :return: nothing
+        """
+        file_name = "temp" + str(index)
+        curr_file = open(file_name, 'w')
+        total_columns = len(self.info_file)
+        start = True
+        if len(self.buffer) == 0:
+            raise NotImplementedError("The buffer size is 0, cannot write to disk")
+        for row in self.buffer:
+            if start:
+                curr_file.write("\n")
+                start = False
+            i = 0
+            for data in row:
+                if i == total_columns - 1:
+                    curr_file.write(str(data))
+                else:
+                    curr_file.write(str(data) + ", ")
+                i += 1
+        self.buffer.clear()
+        
